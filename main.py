8e795a3dfb494ca72b7d4c022bab113455eba077
Daniel <den2002345@yandex.ru>
2022-10-31 16:36:42 +0500

    Update main

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+from telebot.async_telebot import AsyncTeleBot
 import asyncio
-
 import tweepy
 import telebot
-from telebot.async_telebot import AsyncTeleBot
-import ast
 import json
 
 keys = open("twitter_keys.txt").readlines()
@@ -15,8 +13,6 @@ bearer_token = keys[2]
 access_key = keys[3]
 access_secret = keys[4]
 
-client = tweepy.Client(bearer_token, api_key, api_secret, access_key, access_secret)
-
 auth = tweepy.OAuthHandler(api_key, api_secret)
 auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth)
@@ -25,45 +21,8 @@ with open("tg_token.txt") as file:
     tg_token = file.readlines()[0].rstrip("\n")
 tgbot = AsyncTeleBot(tg_token)
 
-# @tgbot.channel_post_handler(content_types=['text'])
-# def handle_chat(message):
-#     global chat_id
-#     if message.text == "/get":
-#         tgbot.send_message(chat_id, "Понял")
-
-
-# @tgbot.message_handler(content_types=['text'])
-# def get_text_messages(message):
-#     global user_id
-#     user_id = message.forward_from_chat.id
-#     if message.text == "/start":
-#         user_id = message.from_user.id
-#         tgbot.send_message(user_id, "Понял! Начинаю работу")
-#     if message.text != "/get":
-#         i = int(message.text)
-#         # tgbot.send_photo(message.from_user.id,
-#         #                  tweets[i].entities["media"][0]["media_url"],
-#         #                  caption=tweets[i].full_text)
-#         media = {}
-#         if "media" in tweets[i].entities \
-#                 and "media" in tweets[i].extended_entities:
-#             media = tweets[i].extended_entities["media"]
-#             photos = [telebot.types.InputMediaPhoto(photo["media_url"]) for photo in media]
-#             photos[0].caption = tweets[i].full_text
-#             tgbot.send_media_group(message.from_user.id, photos)
-#         else:
-#             tgbot.send_message(message.from_user.id, tweets[i].full_text)
-#         # if tweets[2].retweeted:
-#         #     tgbot.send_message(message.from_user.id, tweets[2].retweeted_status.full_text)
-
-bot_json = {"users": [],
-            "chats": [],
-            "user_id_to_signs": {},
-            "chat_id_to_signs": {},
-            "sign_to_since_id": {},
-            "update_offset": 0,
-            "since_id": 0}
-print(bot_json)
+bot_json = {"followers": [],
+            "update_offset": 0}
 
 
 async def resolve_json(file_name: str):
@@ -78,37 +37,40 @@ async def resolve_json(file_name: str):
 
 
 @tgbot.message_handler(commands=["start"])
-async def handle_start(message):
+async def handle_user_start(message):
     user = message.from_user
     await tgbot.send_message(user.id,
-                             "Привет, " + user.first_name + "! Перешли сообщение из канала, в который нужно будет "
-                                                            "отправлять твиты. Так я пойму куда мне это нужно "
-                                                            "делать.\n"
-                                                            "Либо напиши команду /sign, чтобы я отправлял обновления "
-                                                            "с канала тебя напрямую \n"
-                                                            "Но сначала не забудь добавить меня в админы канала!!!")
+                             "Привет, " + user.first_name
+                             + "!\nЧтобы начать отслеживание пользователя воспользуйся "
+                               "коммандой '/add *id твиттера*'.\n"
+                               "Возможно добавление множества пользователей\n"
+                               "Для того, чтобы пользоваться ботом в канале,"
+                               " нужно добавить его в админы канала.")
 
 
-@tgbot.message_handler(commands=["sign"])
-async def sign_user(message):
-    user = message.from_user
-    user_dict = ast.literal_eval(str(user))
-    print(user_dict)
-
-    if user.id not in bot_json["user_id_to_signs"].keys():
-        bot_json["users"].append(user_dict)
-        bot_json["user_id_to_signs"][user.id] = []
-        await tgbot.send_message(user.id, "Теперь я буду отправлять тебе твиты в личные сообщения!")
-        await update_json("bot_json.json", bot_json)
-    else:
-        await tgbot.send_message(user.id, "Ты уже подписан на уведомления!")
-    print(bot_json)
+@tgbot.channel_post_handler(commands=["start"])
+async def handle_chat_start(message):
+    chat = message.chat
+    await tgbot.send_message(chat.id,
+                             "Приветствую всех на канале " + chat.title
+                             + "!\nЧтобы начать отслеживание пользователя воспользуйтесь "
+                               "коммандой '/add *id твиттера*'.\n"
+                               "Возможно добавление множества пользователей!")
 
 
+@tgbot.channel_post_handler(commands=["add"])
 @tgbot.message_handler(commands=["add"])
 async def add_to_signs(message):
     twitter_id = str(message.text).strip("/add ")
-    user = message.from_user
+    if message.from_user is None:
+        user = message.chat
+    else:
+        user = message.from_user
+    if len(twitter_id) == 0:
+        await tgbot.send_message(user.id,
+                                 f'После /add нужно написать id пользователя в твиттер. Этот id можно посмотреть по '
+                                 f'ссылке на его профиль в твиттере')
+        return
 
     try:
         tweets = api.user_timeline(screen_name=twitter_id, count=1)
@@ -117,86 +79,66 @@ async def add_to_signs(message):
         return
 
     if len(tweets) > 0:
-        bot_json["sign_to_since_id"][twitter_id] = tweets[0].id
+        await add_sign(user.id, twitter_id, tweets[0].id)
     else:
-        bot_json["sign_to_since_id"][twitter_id] = 0
+        await add_sign(user.id, twitter_id, 0)
 
-    user = message.from_user
-    user_id = str(user.id)
-    user_dict = ast.literal_eval(str(user))
 
-    if user_id not in bot_json["user_id_to_signs"].keys():
-        bot_json["users"].append(user_dict)
-        bot_json["user_id_to_signs"][user_id] = []
-        await update_json("bot_json.json", bot_json)
+async def add_sign(tg_id, twitter_id, since_id):
+    sign = {"twitter_id": twitter_id,
+            "since_id": since_id}
 
-    if twitter_id not in bot_json["user_id_to_signs"][user_id]:
-        bot_json["user_id_to_signs"][user_id].append(twitter_id)
-        await tgbot.send_message(user.id, f"Теперь я буду отправлять тебе твиты от *{twitter_id}* в личные сообщения!")
-        await update_json("bot_json.json", bot_json)
+    follower = None
+    for foll in bot_json["followers"]:
+        if foll["tg_id"] == tg_id:
+            follower = foll
+    if follower is None:
+        follower = {"tg_id": tg_id,
+                    "signs": []}
+        bot_json["followers"].append(follower)
+
+    twitt_ids = [follower["signs"][sign]["twitter_id"] for sign in range(len(follower["signs"]))]
+    if twitter_id in twitt_ids:
+        await tgbot.send_message(tg_id, f"Вы уже подписаны на уведомления от *{twitter_id}*!")
     else:
-        await tgbot.send_message(user.id, f"Ты уже подписан на уведомления от *{twitter_id}*!")
-    print(bot_json)
+        follower["signs"].append(sign)
+        await tgbot.send_message(tg_id, f"Теперь я буду отправлять сюда твиты от *{twitter_id}*!")
+
+    await update_json("bot_json.json", bot_json)
 
 
-@tgbot.message_handler(content_types=["text"])
-async def sign_chat(message):
-    chat = message.forward_from_chat
-    if chat is None:
-        return
+async def handle_tweets():
+    for follower in bot_json["followers"]:
+        for sign in follower["signs"]:
+            twitter_id = sign["twitter_id"]
+            since_id = sign["since_id"]
+            print(f"Проверка пользователя {twitter_id} для уведомления канала {follower['tg_id']}")
 
-    chat_dict = ast.literal_eval(str(chat))
-
-    if chat_dict["id"] not in bot_json["chat_id_to_signs"].keys():
-        bot_json["chats"].append(chat_dict)
-        bot_json["chat_id_to_signs"][chat_dict["id"]] = []
-        await update_json("bot_json.json", bot_json)
-
-        await tgbot.send_message(chat_dict["id"], "Приятно познакомиться! Теперь я буду высылать твиты в этот канал")
-
-
-async def handle_tweets(tg_ids: list):
-    # TODO Сделать словарь user_id_to_sighs = {"123": ["xeldery", "inskipp"]} и обработку множественной подписки.
-    #  Соответственно нужна возможность отправки id для подписки. for id in ids: передвинуть вверх
-    for tg_id in tg_ids:
-        for signed_id in bot_json["user_id_to_signs"][tg_id]:
-            since_id = bot_json["sign_to_since_id"][signed_id]
-            print("signed_id = " + signed_id)
             if since_id == 0:
-                tweets = api.user_timeline(screen_name=signed_id, tweet_mode="extended", count=1)
+                tweets = api.user_timeline(screen_name=twitter_id, tweet_mode="extended", count=1)
             else:
-                tweets = api.user_timeline(screen_name=signed_id, tweet_mode="extended", since_id=since_id)
+                tweets = api.user_timeline(screen_name=twitter_id, tweet_mode="extended", since_id=since_id)
             for tweet in reversed(tweets):
-                await send_tweet(tg_id, tweet)
-                bot_json["sign_to_since_id"][signed_id] = tweet.id
-                await update_json("bot_json.json", bot_json)
+                await send_tweet(follower["tg_id"], tweet)
+                sign["since_id"] = tweet.id
+
+    await update_json("bot_json.json", bot_json)
 
 
 async def send_tweet(id, tweet):
-    await tgbot.send_message(id,
-                             f"Новый твит от @{tweet._json['user']['screen_name']}\nURL:https://twitter.com/twitter/statuses/{tweet.id}")
+    about_message = f"Новый твит от @{tweet._json['user']['screen_name']}\nURL:https://twitter.com/twitter/statuses/{tweet.id}\n__________\n\n"
     if "media" in tweet.entities \
             and "media" in tweet.extended_entities:
         media = tweet.extended_entities["media"]
         photos = [telebot.types.InputMediaPhoto(photo["media_url"]) for photo in media]
-        photos[0].caption = tweet.full_text
+        photos[0].caption = about_message + tweet.full_text
         await tgbot.send_media_group(id, photos)
     else:
-        await tgbot.send_message(id, tweet.full_text)
-
-
-async def handle_chats(since_id):
-    # await handle_updates(bot_json["chats"])
-    await handle_tweets(bot_json["chat_ids"])
-
-
-async def handle_users(since_id):
-    # await handle_updates(bot_json["users"])
-    await handle_tweets(list(bot_json["user_id_to_signs"].keys()))
+        await tgbot.send_message(id, about_message + tweet.full_text)
 
 
 async def handle_updates():
-    updates = await tgbot.get_updates(offset=bot_json["update_offset"], allowed_updates=["message"], timeout=10)
+    updates = await tgbot.get_updates(offset=bot_json["update_offset"], allowed_updates=["message"], timeout=1)
     if len(updates) > 0:
         bot_json["update_offset"] = updates[len(updates) - 1].update_id + 1
         await update_json("bot_json.json", bot_json)
@@ -206,17 +148,9 @@ async def handle_updates():
 
 async def work():
     while True:
-        print(bot_json["update_offset"])
         print(bot_json)
         await handle_updates()
-        since_id = bot_json["since_id"]
-        # await handle_chats(since_id)
-        await handle_users(since_id)
-
-
-@tgbot.message_handler(commands=["post"])
-async def post(message):
-    api.update_status("How are u today?")
+        await handle_tweets()
 
 
 async def update_json(file_name: str, json_dict: dict):
